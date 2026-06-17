6eba15e5fa456c4832f0f9196965296df35e5567
maheshyadavkosli-sys <maheshyadav.kosli@gmail.com>
2026-06-17 10:59:59 -0500

    6#	modified:   10-storage-account.tf

--- 10-storage-account.tf
+++ 10-storage-account.tf
@@ -10,13 +10,13 @@ resource "azurerm_storage_account" "st_name" {
 resource "azurerm_storage_container" "container1" {
   for_each              = local.location_code
   name                  = lower("container-${local.naming_suffix}")
-  storage_account_name  = azurerm_storage_account.st_name[each.key].name
+  storage_account_id  = azurerm_storage_account.st_name[each.key].id
   container_access_type = "private"
 }
 resource "azurerm_storage_blob" "blob1" {
   for_each             = local.location_code
   name                 = lower("blob-${local.naming_suffix}")
-  storage_account_name = azurerm_storage_account.st_name[each.key].name
+  storage_account_id = azurerm_storage_account.st_name[each.key].id
   storage_container_id = azurerm_storage_container.container1[each.key].id
   type                 = "Block"
   source_content       = "This is a sample blob content for ${local.naming_suffix}"
