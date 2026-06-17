7ffbf38ea3f09f4535dd08588826cbc144aee999
maheshyadavkosli-sys <maheshyadav.kosli@gmail.com>
2026-06-17 12:49:02 -0500

    11

--- 05-locals.tf
+++ 05-locals.tf
@@ -5,14 +5,12 @@ locals {
     { code = "AF", location = "southafricanorth", business_unit = "FIN", environment = "QA" },
     { code = "JW", location = "japanwest", business_unit = "FIN", environment = "QA" },
   ]
-
   # 2. Automatically transform the list into a lookup map: {"southafricanorth" = "AF", ...}
   location_code = { for item in local.location : item.location => item.code }
 
   # 3. Perform your lookup and naming suffix generation
   naming_suffix = "${local.location[0].business_unit}-${local.location[0].environment}"
 }
-
 /*[
     { code = "AF", location = "southafricanorth" },
     { code = "AE", location = "australiaeast" },
