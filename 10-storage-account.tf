4613257e34df370ac5a221a74246516f2a21edb1
maheshyadavkosli-sys <maheshyadav.kosli@gmail.com>
2026-06-17 10:57:53 -0500

    5

--- 10-storage-account.tf
+++ 10-storage-account.tf
@@ -1,23 +1,23 @@
 resource "azurerm_storage_account" "st_name" {
-    for_each = local.location_code
-  name                     = "substring(st${random_string.random8.result}${local.location[0].location }, 0, 24)"
+  for_each                 = local.location_code
+  name                     = substr("storage${random_string.random8.result}${local.location[0].location}", 0, 24)
   resource_group_name      = azurerm_resource_group.rg[each.key].name
   location                 = azurerm_resource_group.rg[each.key].location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  
+
 }
 resource "azurerm_storage_container" "container1" {
-    for_each = local.location_code
+  for_each              = local.location_code
   name                  = lower("container-${local.naming_suffix}")
   storage_account_name  = azurerm_storage_account.st_name[each.key].name
   container_access_type = "private"
 }
 resource "azurerm_storage_blob" "blob1" {
-    for_each = local.location_code
-  name                   = lower("blob-${local.naming_suffix}")
-  storage_account_name   = azurerm_storage_account.st_name[each.key].name
+  for_each             = local.location_code
+  name                 = lower("blob-${local.naming_suffix}")
+  storage_account_name = azurerm_storage_account.st_name[each.key].name
   storage_container_id = azurerm_storage_container.container1[each.key].id
-  type                   = "Block"
-  source_content         = "This is a sample blob content for ${local.naming_suffix}"
+  type                 = "Block"
+  source_content       = "This is a sample blob content for ${local.naming_suffix}"
 }
