e77dab66d4144420befa53571fa64f01229633a1
Mahesh <maheshyadav.kosli@gmail.com>
2026-06-19 10:56:43 -0500

    sensitive

--- 04-output.tf
+++ 04-output.tf
@@ -1,8 +1,9 @@
 output "naming_suffix" {
   value       = local.naming_suffix
   description = "The generated naming suffix based on the first location's business unit and environment."
-
+  sensitive = true
 }
+/*
 output "location_code_map" {
   value       = local.location_code
   description = "A map of Azure locations to their corresponding codes."
@@ -15,3 +16,4 @@ output "storage_account_names" {
   value       = { for loc in local.location : loc.location => "st${random_string.random8.result}${loc.location}" }
   description = "A map of Azure locations to their corresponding storage account names."
 }
+*/
